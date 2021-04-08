705b4bc811772176da26f034f55ab9d5806069b5
ravikumar <ravikumar@gratian.tech>
2021-04-08 16:47:33 +0530

    chf cookbooks update

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -6,9 +6,27 @@ end
 ## If no action is specified, a default action for specified resource 
 ## is applied automatically
 package 'ntp' 
+package 'git'
 
+##use node object to get ohai information
+#node['ipadress']
+#node['memory']['total']
+
+## print 'I have 4 apples'
+apple_count = 4
+puts "I have #{apple_count} apples"
+
+##get ohai information into the motd file
 file '/etc/motd' do
-    content 'This message was generated from chef recipe...'
+    content "This message was generated from chef recipe...
+    HOSTNAME: #{node['hostname']}
+    IPADDRESS: #{node['ipaddress']}
+    CPU: #{node['cpu']['0']['mhz']}
+    MEMORY: #{node['memory']['total']}
+"
+    owner 'root'
+    group 'root'
+    action :create
 end
 
 service 'ntp' do
